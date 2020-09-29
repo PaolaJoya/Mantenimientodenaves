908d3f3b4c86121d8146e5cd865c20161e8a8e98
Paola Joya <paolajoya24@gmail.com>
2020-09-29 17:39:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mantenimiendo_de_naves/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mantenimiendo_de_naves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
