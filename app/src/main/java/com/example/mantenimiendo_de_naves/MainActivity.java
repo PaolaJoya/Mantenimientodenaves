77d5ffe91cd12f1ce9f9da689996cb5f57538f28
Paola Joya <paolajoya24@gmail.com>
2020-10-02 11:32:17 -0500

    2

--- app/src/main/java/com/example/mantenimiendo_de_naves/MainActivity.java
+++ app/src/main/java/com/example/mantenimiendo_de_naves/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.mantenimiendo_de_naves;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void irARegistro (View view){
+        Intent i =new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
