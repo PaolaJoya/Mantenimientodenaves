0bac0baf032568b50042829008f2870968df918f
Paola Joya <paolajoya24@gmail.com>
2020-10-22 19:06:15 -0500

    menu

--- app/src/main/java/com/example/mantenimiendo_de_naves/Registro.java
+++ app/src/main/java/com/example/mantenimiendo_de_naves/Registro.java
@@ -1,7 +1,9 @@
 package com.example.mantenimiendo_de_naves;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,64 +11,112 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+
 public class Registro extends AppCompatActivity {
 
     private EditText nombre1;
     private EditText apellido1;
     private EditText correo1;
     private EditText confcorreo1;
-    private EditText contraseña1;
-    private EditText confcontraseña1;
+    private EditText contrasena1;
+    private EditText confcontrasena1;
     private EditText tipodocumento1;
     private EditText documento1;
     private EditText telefono1;
     private EditText codigo1;
+    private ProgressDialog progressDialog;
 
 
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         nombre1=(EditText)findViewById(R.id.nombres);
         apellido1=(EditText)findViewById(R.id.apellidos);
         correo1=(EditText)findViewById(R.id.correo);
         confcorreo1=(EditText)findViewById(R.id.confcorreo);
-        contraseña1=(EditText)findViewById(R.id.contraseña);
-        confcontraseña1=(EditText)findViewById(R.id.contraseña);
+        contrasena1=(EditText)findViewById(R.id.contraseña);
+        confcontrasena1=(EditText)findViewById(R.id.contraseña);
         //tipodocumento1=(EditText)findViewById(R.id.tipodocumento);
         documento1=(EditText)findViewById(R.id.documento);
         telefono1=(EditText)findViewById(R.id.telefono);
         codigo1=(EditText)findViewById(R.id.codigo);
 
 
+
     }
 
     public void Registrar(View vi){
 
-        if(validar()){
-            Toast.makeText(this,"Ha registrado sus datos con exito",Toast.LENGTH_SHORT).show();
+        String correo2=correo1.getText().toString();
+        String correo22=confcorreo1.getText().toString();
+        String contrasena2=contrasena1.getText().toString();
+        String contrasena22=confcontrasena1.getText().toString();
 
-            Intent main= new Intent(this, MainActivity.class);
-            startActivity(main);
+        if(validar()) {
+            if(correo2.equals(correo22)){
+                if (contrasena2.equals(contrasena22)) {
+                mAuth.createUserWithEmailAndPassword(correo2, contrasena2)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    //Log.d(TAG, "createUserWithEmail:success");
+
+                                    //Log.d(TAG, "Ha registrado sus datos con exito");
+                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent menuinicio = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(menuinicio);
+                                    //updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Autenticación fallida", Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                            }
+                        });
+
+                }
+            }
+        }else{
+            Toast.makeText(this,"Uno o varios de los campos registrados, son erroneos",Toast.LENGTH_SHORT).show();
         }
+
     }
 
     public boolean validar(){
 
         boolean retorno= true;
-
         String nombre2=nombre1.getText().toString();
         String apellido2=apellido1.getText().toString();
         String correo2=correo1.getText().toString();
         String correo22=confcorreo1.getText().toString();
-        String contrasena2=contraseña1.getText().toString();
-        String contrasena22=confcontraseña1.getText().toString();
+        String contrasena2=contrasena1.getText().toString();
+        String contrasena22=confcontrasena1.getText().toString();
         //String tpodocumento2=tipodocumento1.getText().toString();
         String documento2=documento1.getText().toString();
         String telefono2=telefono1.getText().toString();
         String codigo2=codigo1.getText().toString();
+
 
 
         if (nombre2.isEmpty()) {
@@ -90,12 +140,12 @@ public class Registro extends AppCompatActivity {
         }
 
         if (contrasena2.isEmpty()) {
-            contraseña1.setError("Este campo es obligatorio");
+            contrasena1.setError("Este campo es obligatorio");
             retorno = false;
         }
 
         if (contrasena22.isEmpty()) {
-            confcontraseña1.setError("Este campo es obligatorio");
+            confcontrasena1.setError("Este campo es obligatorio");
             retorno = false;
         }
 
@@ -121,4 +171,14 @@ public class Registro extends AppCompatActivity {
 
         return retorno;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
